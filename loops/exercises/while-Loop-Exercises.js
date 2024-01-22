0c3ff2a678c1c31c902fca6f4b29c088600d100d
shellywilliamsreichert802 <154470623+shellywilliamsreichert802@users.noreply.github.com>
2024-01-21 19:12:52 -0600

    while loop exercise  part a

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,34 +1,24 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+const input = require('readline-sync');
 
-startingFuel
-numOfAstronautsAboard
-shuttleAltitude
+let startingFuelLevel = Number(input.question("Enter starting fuel level: "));
+let astronautsAboard = "";
+let shuttleAltitude = "";
 
 
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-  const input = require('readline-sync');
 
-  let info = input.question("Question text... ");
-  
-  let numbers = [ /* some numbers */ ];
-  let searchVal = 42;
-  let i = 0;
-  
-  while (i < numbers.length) {
-     if (numbers[i] === searchVal) {
-        break;
-     }
-     i++;
-  }
-  
-  if (i < numbers.length) {
-     console.log("The value", searchVal, "was located at index", i);
-  } else {
-     console.log("The value", searchVal, "is not in the array.");
-  }
-
+while (startingFuelLevel < 5000 || startingFuelLevel > 30000 || isNaN(startingFuelLevel)) {
+   if (isNaN(startingFuelLevel)) {
+      startingFuelLevel = input.question ('Please enter a number: ');
+   } else if (startingFuelLevel < 5000) {
+         startingFuelLevel = input.question('Below 5000 is not allowed. Reenter a number: ');
+   } else if (startingFuelLevel > 30000) {
+      startingFuelLevel = input.question('Over 30000 not allowed. Reenter a number: ');
+   }
+}
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
