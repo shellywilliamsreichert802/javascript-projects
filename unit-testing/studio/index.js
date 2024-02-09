e4fe6d474995354d649fc2e1dad63ffa64ee782c
shellywilliamsreichert802 <154470623+shellywilliamsreichert802@users.noreply.github.com>
2024-02-08 20:37:06 -0600

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,33 @@
 
-let launchcode = {
+// let launchcode = {
   
-}
+// }
 
+// launchcode.test.js code: 
+// const launchcode = require('../index.js'); 
+// describe("Testing launchcode", function(){ 
+//   // Write your unit tests here! 
+//   it("should have the correct properties and values", function(){
+//     expect(launchcode.organization).toBe("nonprofit");
+//     expect(launchcode.executiveDirector).toBe("Jeff");
+//     expect(launchcode.percentageCoolEmployees).toBe(100);
+//     expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+//   });
+//   it("should have a launchOutput method that returns a string", function(){
+//     expect(typeof launchcode.launchOutput).toBe("function");
+//     expect(typeof launchcode.launchOutput()).toBe("string");
+//   });
+// });
+
+const launchcode = 
+{
+    organization: 'nonprofit',
+    executiveDirector: 'Jeff',
+    percentageCoolEmployees: 100,
+    programsOffered: ['Web Development', 'Data Analysis', 'Liftoff'],
+    launchOutput: function(){
+      return `LaunchCode is a ${this.organization} that offers ${this.programsOffered.join(', ')} programs. The executive director is ${this.executiveDirector} and ${this.percentageCoolEmployees}% of the employees are cool.`;
+    }
+  };
+  
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,40 @@
-// launchcode.test.js code:
-const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+// launchcode.test.js code: 
+const launchcode = require('../index.js'); 
+const assert = require('assert');
 
-  // Write your unit tests here!
+describe("Testing launchcode", function()
+{ 
+  // Write your unit tests here! 
+  // it("should have the correct organization property with value 'nonprofit'", function(){
+  //   expect(launchcode.organization).toEqual("nonprofit");
+  // });
+
+  it("should have organization property with value 'nonprofit'", function(){
+    assert.equal(launchcode.organization, 'nonprofit');
+  });
+  it("should have executiveDirector property with value 'Jeff'", function(){
+    assert.equal(launchcode.executiveDirector, 'Jeff');
+  });
+  it("should have the correct percentageCoolEmployees", function(){
+        assert.equal(launchcode.percentageCoolEmployees, '100');
+      });
   
-});
+  // it("should have executiveDirector property with value 'Jeff'", function(){
+  //   expect(launchcode.executiveDirector).toEqual('Jeff');
+  // });
+  // Add more tests for other properties and methods here
+});  
+//   it("should have the correct executiveDirector", function(){
+//     expect(launchcode.executiveDirector).toEqual("Jeff");
+//   });
+//   it("should have the correct percentageCoolEmployees", function(){
+//     expect(launchcode.percentageCoolEmployees).toEqual(100);
+//   });
+//   it("should have the correct programsOffered", function(){
+//     expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+//   });
+//   it("should have a launchOutput method that returns a string", function(){
+//     expect(typeof launchcode.launchOutput()).toEqual("string");
+// //   });
+// });
